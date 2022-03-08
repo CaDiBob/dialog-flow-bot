dc6f35cdc55fca61a21f609bcbcca3bc1e42739e
CaDiBob <93527781+CaDiBob@users.noreply.github.com>
2022-03-08 22:45:48 +0900

    Добавил функцию старт и эхо

--- bot.py
+++ bot.py
@@ -1,6 +1,9 @@
 import os
 import logging
 
+
+from telegram import Update, ForceReply
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
 from environs import Env
 
 
@@ -11,12 +14,27 @@ logging.basicConfig(
 logger = logging.getLogger('bot')
 
 
+def start(update, context):
+    user = update.effective_user
+    update.message.reply_text('Hello!')
+
+
+def echo(update, context):
+    update.message.reply_text(update.message.text)
+
 
 def main():
     env = Env()
     env.read_env('.env')
     tg_token = env('TG_TOKEN')
-    print(tg_token)
+    updater = Updater(tg_token)
+    dispatcher = updater.dispatcher
+    dispatcher.add_handler(CommandHandler("start", start))
+    dispatcher.add_handler(MessageHandler(
+        Filters.text & ~Filters.command, echo
+    ))
+    updater.start_polling()
+    updater.idle()
 
 
 if __name__ == '__main__':
