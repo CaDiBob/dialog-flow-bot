37e2a873cec7f2387bb755a1a624afd2a3912266
CaDiBob <93527781+CaDiBob@users.noreply.github.com>
2022-03-16 21:50:49 +0900

    Добавил обработку исключений

--- tg_bot.py
+++ tg_bot.py
@@ -1,18 +1,25 @@
-import os
 import logging
+import time
 
 import telegram
-from google.cloud import dialogflow
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
 from environs import Env
-from dialog_flow import answer_dialogflow
+from dialog_flow import get_answer_dialogflow
 
-
-logging.basicConfig(
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
-)
 
 logger = logging.getLogger('bot')
+
+
+class TelegramLogsHandler(logging.Handler):
+
+    def __init__(self, tg_chat_id, bot):
+        super().__init__()
+        self.chat_id = tg_chat_id
+        self.tg_bot = bot
+
+    def emit(self, record):
+        log_entry = self.format(record)
+        self.tg_bot.send_message(chat_id=self.chat_id, text=log_entry)
 
 
 def start(update, context):
@@ -20,10 +27,18 @@ def start(update, context):
 
 
 def send_message(update, context):
-    project_id = context.bot_data['project_id']
-    text = update.message.text
-    chat_id = update.message.chat_id
-    update.message.reply_text(answer_dialogflow(project_id, text, chat_id))
+    try:
+        project_id = context.bot_data['project_id']
+        text = update.message.text
+        chat_id = update.message.chat_id
+        answer = get_answer_dialogflow(project_id, text, chat_id)
+        if answer:
+            update.message.reply_text(answer)
+    except telegram.error.BadRequest as error:
+        logger.exception(error)
+    except telegram.error.NetworkError as error:
+        time.sleep(90)
+        logger.exception(error)
 
 
 def main():
@@ -34,6 +49,8 @@ def main():
     tg_chat_id = env('TG_CHAT_ID')
     updater = Updater(tg_token)
     bot = telegram.Bot(token=tg_token)
+    logger.addHandler(TelegramLogsHandler(tg_chat_id, bot))
+    logger.info('Бот запущен!')
     dispatcher = updater.dispatcher
     dispatcher.bot_data['project_id'] = project_id
     dispatcher.add_handler(CommandHandler("start", start))
