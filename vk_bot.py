2f66f8068c2ee8ee3d63548667c5b115a0b31572
CaDiBob <93527781+CaDiBob@users.noreply.github.com>
2022-03-16 19:47:30 +0900

    Подключил бота VK к dialogflow

--- vk_bot.py
+++ vk_bot.py
@@ -1,16 +1,17 @@
-
-import random
-
 import vk_api as vk
 from vk_api.longpoll import VkLongPoll, VkEventType
 from vk_api.utils import get_random_id
 from environs import Env
+from dialog_flow import answer_dialogflow
 
 
-def echo(event, vk_api):
+def send_message(event, vk_api, project_id):
+    text = event.text
+    chat_id = event.user_id
+    answer = answer_dialogflow(project_id, text, chat_id)
     vk_api.messages.send(
         user_id=event.user_id,
-        message=event.text,
+        message=answer,
         random_id=get_random_id()
     )
 
@@ -19,12 +20,13 @@ def main():
     env = Env()
     env.read_env('.env')
     vk_token = env('VK_TOKEN')
+    project_id = env('PROJECT_ID')
     vk_session = vk.VkApi(token=vk_token)
     vk_api = vk_session.get_api()
     longpoll = VkLongPoll(vk_session)
     for event in longpoll.listen():
         if event.type == VkEventType.MESSAGE_NEW and event.to_me:
-            echo(event, vk_api)
+            send_message(event, vk_api, project_id)
 
 
 if __name__ == "__main__":
