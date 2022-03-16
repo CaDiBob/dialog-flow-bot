20f1038c59b859f2102ea05b43a64f4b8a8cb808
CaDiBob <93527781+CaDiBob@users.noreply.github.com>
2022-03-16 21:49:38 +0900

    Исправил создание интентов

--- create_intent.py
+++ create_intent.py
@@ -4,23 +4,19 @@ from google.cloud import dialogflow
 from environs import Env
 
 
-def create_intent(project_id, display_name, training_phrases_parts, message_texts):
+def create_intent(project_id, display_name, training_phrases_parts, message_text):
+    text = dialogflow.Intent.Message.Text(text=message_text)
+    message = dialogflow.Intent.Message(text=text)
     intents_client = dialogflow.IntentsClient()
     parent = dialogflow.AgentsClient.agent_path(project_id)
     training_phrases = []
     for training_phrases_part in training_phrases_parts:
         part = dialogflow.Intent.TrainingPhrase.Part(text=training_phrases_part)
-        # Here we create a new training phrase for each provided part.
         training_phrase = dialogflow.Intent.TrainingPhrase(parts=[part])
         training_phrases.append(training_phrase)
-
-    text = dialogflow.Intent.Message.Text(text=message_texts)
-    message = dialogflow.Intent.Message(text=text)
-
     intent = dialogflow.Intent(
         display_name=display_name, training_phrases=training_phrases, messages=[message]
     )
-
     response = intents_client.create_intent(
         request={"parent": parent, "intent": intent}
     )
@@ -34,7 +30,7 @@ def main():
     project_id = env.str('PROJECT_ID')
     for intent_name, phrases in questions.items():
         training_phrases_parts = phrases['questions']
-        message_text = phrases['answer']
+        message_text = [phrases['answer']]
         create_intent(project_id, intent_name,  training_phrases_parts, message_text,)
 
 
