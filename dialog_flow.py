482c72d9f8045a05ef5adb69866e4f4fd04b82bf
CaDiBob <93527781+CaDiBob@users.noreply.github.com>
2022-03-16 21:52:53 +0900

    Изменил условие

--- dialog_flow.py
+++ dialog_flow.py
@@ -11,6 +11,5 @@ def get_answer_dialogflow(project_id, text, chat_id):
     response = session_client.detect_intent(
         request={"session": session, "query_input": query_input}
     )
-    if response.query_result.intent.is_fallback:
-        return
-    return response.query_result.fulfillment_text
+    if not response.query_result.intent.is_fallback:
+        return response.query_result.fulfillment_text
