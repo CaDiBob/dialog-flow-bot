00e07cafc0ed555d6c6f137f317a877acb8634ef
CaDiBob <93527781+CaDiBob@users.noreply.github.com>
2022-03-16 21:50:12 +0900

    Изменил имя функции

--- dialog_flow.py
+++ dialog_flow.py
@@ -1,7 +1,7 @@
 from google.cloud import dialogflow
 
 
-def answer_dialogflow(project_id, text, chat_id):
+def get_answer_dialogflow(project_id, text, chat_id):
     session_client = dialogflow.SessionsClient()
     session = session_client.session_path(
         project_id, session=chat_id,
@@ -11,4 +11,6 @@ def answer_dialogflow(project_id, text, chat_id):
     response = session_client.detect_intent(
         request={"session": session, "query_input": query_input}
     )
+    if response.query_result.intent.is_fallback:
+        return
     return response.query_result.fulfillment_text
